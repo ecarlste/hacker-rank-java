72299bc6eddf45333cf55d311ca62f7da8b85ea7
Erik S Carlsten <ecarlste@gmail.com>
2017-02-15 16:34:59 -0600

    Created method to build value array from STDIN

--- java-negative-subarray/src/Solution.java
+++ java-negative-subarray/src/Solution.java
@@ -4,6 +4,26 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int[] values = buildValuesArrayFromStdin();
+
+        int negativeSubarrayCount = getNegativeSubarrayCount(values);
+    }
+
+    private static int[] buildValuesArrayFromStdin() {
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+
+        int[] values = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        return values;
+    }
+
+    private static int getNegativeSubarrayCount(int[] values) {
+        return 0;
     }
 }

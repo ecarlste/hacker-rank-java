c1920faa1fb22741badb8ad4fb8b800ac884fc0e
Erik S Carlsten <ecarlste@gmail.com>
2017-03-06 14:59:21 -0600

    Added QueryType enum and method to set QueryType from the scanner

--- java-list/src/Solution.java
+++ java-list/src/Solution.java
@@ -2,12 +2,48 @@ import java.util.*;
 
 public class Solution {
 
+    private enum QueryType {
+        INSERT("Insert"),
+        DELETE("Delete");
+
+        private final String name;
+
+        QueryType(String name) {
+            this.name = name;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         List<Integer> elements = buildElementListFromScanner(scanner);
 
+        elements = performAllQueriesOnElementListFromScanner(elements, scanner);
+
+    }
+
+    private static List<Integer> performAllQueriesOnElementListFromScanner(List<Integer> elements, Scanner scanner) {
+        int queryCount = scanner.nextInt();
+
+        for (int i = 0; i < queryCount; i++) {
+            QueryType queryType = getQueryTypeFromScanner(scanner);
+        }
+
+        return null;
+    }
+
+    private static QueryType getQueryTypeFromScanner(Scanner scanner) {
+        QueryType queryType;
+        String queryTypeString = scanner.next();
+
+        if (queryTypeString.equals("Insert")) {
+            queryType = QueryType.INSERT;
+        } else {
+            queryType = QueryType.DELETE;
+        }
+
+        return queryType;
     }
 
     private static List<Integer> buildElementListFromScanner(Scanner scanner) {

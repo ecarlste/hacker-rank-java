fb43304d2f698950c470bf5473afa85dd5f2a512
Erik S Carlsten <ecarlste@gmail.com>
2017-03-07 10:46:16 -0600

    Initial solution which handles sample input provided

--- java-stack/src/Solution.java
+++ java-stack/src/Solution.java
@@ -5,7 +5,7 @@ class Solution {
     private enum InputTokenType {
         PARENTHESES, CURLY_BRACE, SQUARE_BRACE, INVALID;
 
-        public InputTokenType getInputTokenTypeForCharacter(String token) {
+        public static InputTokenType getInputTokenTypeForCharacter(String token) {
             switch (token) {
                 case "(":
                 case ")":
@@ -28,9 +28,49 @@ class Solution {
         while (sc.hasNext()) {
             String input = sc.next();
 
-            System.out.println(input);
-            //Complete the code
+            List<String> tokens = splitInputStringIntoSingleTokenStrings(input);
+
+            boolean isBalancedTokenList = checkTokenListForBalance(tokens);
+
+            System.out.println(isBalancedTokenList);
         }
 
     }
+
+    private static List<String> splitInputStringIntoSingleTokenStrings(String input) {
+        String[] tokenArray = input.split("(?!^)");
+
+        return Arrays.asList(tokenArray);
+    }
+
+    private static boolean checkTokenListForBalance(List<String> tokens) {
+        Stack<InputTokenType> tokenTypeStack = new Stack<>();
+
+        for (String token : tokens) {
+            InputTokenType tokenType = InputTokenType.getInputTokenTypeForCharacter(token);
+
+            if (isOpeningToken(token)) {
+                tokenTypeStack.push(tokenType);
+            }
+
+            if (isClosingToken(token)) {
+                InputTokenType lastOpeningToken = tokenTypeStack.pop();
+
+                if (tokenType != lastOpeningToken) {
+                    return false;
+                }
+            }
+        }
+
+        return tokenTypeStack.isEmpty();
+    }
+
+    private static boolean isOpeningToken(String token) {
+        return token.equals("(") || token.equals("{") || token.equals("[");
+    }
+
+    private static boolean isClosingToken(String token) {
+        return token.equals(")") || token.equals("}") || token.equals("]");
+    }
+
 }

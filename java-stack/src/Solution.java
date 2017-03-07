abb4e04aec7076ecbb27e9dcfc4d9e6a358be3d3
Erik S Carlsten <ecarlste@gmail.com>
2017-03-06 19:11:39 -0600

    Added enumerated type for input token matching

--- java-stack/src/Solution.java
+++ java-stack/src/Solution.java
@@ -1,12 +1,34 @@
 import java.util.*;
-class Solution{
 
-    public static void main(String []argh)
-    {
+class Solution {
+
+    private enum InputTokenType {
+        PARENTHESES, CURLY_BRACE, SQUARE_BRACE, INVALID;
+
+        public InputTokenType getInputTokenTypeForCharacter(String token) {
+            switch (token) {
+                case "(":
+                case ")":
+                    return PARENTHESES;
+                case "{":
+                case "}":
+                    return CURLY_BRACE;
+                case "[":
+                case "]":
+                    return SQUARE_BRACE;
+            }
+
+            return INVALID;
+        }
+    }
+
+    public static void main(String []argh) {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String input=sc.next();
+            String input = sc.next();
+
+            System.out.println(input);
             //Complete the code
         }
 

0da7342a6f2084df49f853cf549047d151228d31
Erik S Carlsten <ecarlste@gmail.com>
2017-03-06 18:34:10 -0600

    Added methods to scan all phone book entries and searches

--- phone-book/src/Solution.java
+++ phone-book/src/Solution.java
@@ -1,22 +1,49 @@
-//Complete this code or write your own from scratch
+import java.io.InputStream;
 import java.util.*;
-import java.io.*;
 
 class Solution{
+
+    private static Map<String, Integer> phoneBook = new HashMap<>();
+    private static List<String> namesToSearch = new ArrayList<>();
+
     public static void main(String []argh)
     {
-        Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        in.nextLine();
-        for(int i=0;i<n;i++)
-        {
-            String name=in.nextLine();
-            int phone=in.nextInt();
-            in.nextLine();
-        }
-        while(in.hasNext())
-        {
-            String s=in.nextLine();
+        buildPhoneBookAndNamesToSearch();
+    }
+
+    private static void buildPhoneBookAndNamesToSearch() {
+        Scanner scanner = new Scanner(System.in);
+        buildPhoneBookFromScanner(scanner);
+        buildNamesToSearchFromScanner(scanner);
+        scanner.close();
+    }
+
+    private static void buildPhoneBookFromScanner(Scanner scanner) {
+        int phoneBookEntryCount = scanPhoneBookEntryCount(scanner);
+
+        for (int i = 0; i < phoneBookEntryCount; i++) {
+            scanNameAndNumberAddToPhoneBook(scanner);
         }
     }
+
+    private static int scanPhoneBookEntryCount(Scanner scanner) {
+        int phoneBookEntryCount = scanner.nextInt();
+        scanner.nextLine();
+        return phoneBookEntryCount;
+    }
+
+    private static void scanNameAndNumberAddToPhoneBook(Scanner scanner) {
+        String name = scanner.nextLine();
+        int phoneNumber = scanner.nextInt();
+        scanner.nextLine();
+        phoneBook.put(name, phoneNumber);
+    }
+
+    private static void buildNamesToSearchFromScanner(Scanner scanner) {
+        while (scanner.hasNext()) {
+            String nameToSearch = scanner.nextLine();
+            namesToSearch.add(nameToSearch);
+        }
+    }
+
 }

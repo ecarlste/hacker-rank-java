ecdfb3684b596415b2925bdbdb8c1a0843b3d619
Erik S Carlsten <ecarlste@gmail.com>
2017-03-09 14:25:45 -0600

    First solution that passes provided test case

--- java-bitset/src/Solution.java
+++ java-bitset/src/Solution.java
@@ -1,12 +1,40 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.BitSet;
+import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scanner = new Scanner(System.in);
+
+        int bitSetSize = scanner.nextInt();
+        int binaryOperationCount = scanner.nextInt();
+
+        BitSet[] bitSets = new BitSet[2];
+        bitSets[0] = new BitSet(bitSetSize);
+        bitSets[1] = new BitSet(bitSetSize);
+
+        for (int i = 0; i < binaryOperationCount; i++) {
+            String binaryOperationType = scanner.next();
+            int arg1 = scanner.nextInt();
+            int arg2 = scanner.nextInt();
+
+            if (binaryOperationType.equals("AND")) {
+                bitSets[arg1 - 1].and(bitSets[arg2 - 1]);
+            } else if (binaryOperationType.equals("OR")) {
+                bitSets[arg1 - 1].or(bitSets[arg2 - 1]);
+            } else if (binaryOperationType.equals("XOR")) {
+                bitSets[arg1 - 1].xor(bitSets[arg2 - 1]);
+            } else if (binaryOperationType.equals("FLIP")) {
+                bitSets[arg1 - 1].flip(arg2);
+            } else {
+                bitSets[arg1 - 1].set(arg2);
+            }
+
+            System.out.println(bitSets[0].cardinality() + " " + bitSets[1].cardinality());
+        }
+
+        scanner.close();
+
     }
 }

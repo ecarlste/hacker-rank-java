18fd42411411b0e3638ea131245aa85e6e0df854
Erik S Carlsten <ecarlste@gmail.com>
2017-03-09 14:26:28 -0600

    Refactored if statement into switch for operator types

--- java-bitset/src/Solution.java
+++ java-bitset/src/Solution.java
@@ -19,16 +19,22 @@ public class Solution {
             int arg1 = scanner.nextInt();
             int arg2 = scanner.nextInt();
 
-            if (binaryOperationType.equals("AND")) {
-                bitSets[arg1 - 1].and(bitSets[arg2 - 1]);
-            } else if (binaryOperationType.equals("OR")) {
-                bitSets[arg1 - 1].or(bitSets[arg2 - 1]);
-            } else if (binaryOperationType.equals("XOR")) {
-                bitSets[arg1 - 1].xor(bitSets[arg2 - 1]);
-            } else if (binaryOperationType.equals("FLIP")) {
-                bitSets[arg1 - 1].flip(arg2);
-            } else {
-                bitSets[arg1 - 1].set(arg2);
+            switch (binaryOperationType) {
+                case "AND":
+                    bitSets[arg1 - 1].and(bitSets[arg2 - 1]);
+                    break;
+                case "OR":
+                    bitSets[arg1 - 1].or(bitSets[arg2 - 1]);
+                    break;
+                case "XOR":
+                    bitSets[arg1 - 1].xor(bitSets[arg2 - 1]);
+                    break;
+                case "FLIP":
+                    bitSets[arg1 - 1].flip(arg2);
+                    break;
+                default:
+                    bitSets[arg1 - 1].set(arg2);
+                    break;
             }
 
             System.out.println(bitSets[0].cardinality() + " " + bitSets[1].cardinality());

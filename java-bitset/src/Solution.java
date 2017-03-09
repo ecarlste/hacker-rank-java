61b0423541821d298daf28059206f7b258780c74
Erik S Carlsten <ecarlste@gmail.com>
2017-03-09 14:48:55 -0600

    Extracted multiple methods to allow for lower complexity

--- java-bitset/src/Solution.java
+++ java-bitset/src/Solution.java
@@ -4,43 +4,83 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        BitSet[] bitSets = buildBitSetsFromScanner(scanner);
+        scanProcessAndPrintResultsForAllBitSetOperations(scanner, bitSets);
+        scanner.close();
+    }
 
-        int bitSetSize = scanner.nextInt();
+    private static void scanProcessAndPrintResultsForAllBitSetOperations(Scanner scanner, BitSet[] bitSets) {
         int binaryOperationCount = scanner.nextInt();
+
+        for (int i = 0; i < binaryOperationCount; i++) {
+            Solution.BinaryOperation binaryOperation = buildBinaryOperationFromScanner(scanner);
+
+            performBinaryOperationOnBitSets(binaryOperation, bitSets);
+
+            System.out.println(bitSets[0].cardinality() + " " + bitSets[1].cardinality());
+        }
+    }
+
+    private static BitSet[] buildBitSetsFromScanner(Scanner scanner) {
+        int bitSetSize = scanner.nextInt();
 
         BitSet[] bitSets = new BitSet[2];
         bitSets[0] = new BitSet(bitSetSize);
         bitSets[1] = new BitSet(bitSetSize);
+        return bitSets;
+    }
 
-        for (int i = 0; i < binaryOperationCount; i++) {
-            String binaryOperationType = scanner.next();
-            int arg1 = scanner.nextInt();
-            int arg2 = scanner.nextInt();
+    private static Solution.BinaryOperation buildBinaryOperationFromScanner(Scanner scanner) {
+        String binaryOperationType = scanner.next();
+        int arg1 = scanner.nextInt();
+        int arg2 = scanner.nextInt();
 
-            switch (binaryOperationType) {
-                case "AND":
-                    bitSets[arg1 - 1].and(bitSets[arg2 - 1]);
-                    break;
-                case "OR":
-                    bitSets[arg1 - 1].or(bitSets[arg2 - 1]);
-                    break;
-                case "XOR":
-                    bitSets[arg1 - 1].xor(bitSets[arg2 - 1]);
-                    break;
-                case "FLIP":
-                    bitSets[arg1 - 1].flip(arg2);
-                    break;
-                default:
-                    bitSets[arg1 - 1].set(arg2);
-                    break;
-            }
+        return new Solution.BinaryOperation(binaryOperationType, arg1, arg2);
+    }
 
-            System.out.println(bitSets[0].cardinality() + " " + bitSets[1].cardinality());
+    private static void performBinaryOperationOnBitSets(Solution.BinaryOperation binaryOperation, BitSet[] bitSets) {
+        switch (binaryOperation.getBinaryOperationType()) {
+            case "AND":
+                bitSets[binaryOperation.getArg1() - 1].and(bitSets[binaryOperation.getArg2() - 1]);
+                break;
+            case "OR":
+                bitSets[binaryOperation.getArg1() - 1].or(bitSets[binaryOperation.getArg2() - 1]);
+                break;
+            case "XOR":
+                bitSets[binaryOperation.getArg1() - 1].xor(bitSets[binaryOperation.getArg2() - 1]);
+                break;
+            case "FLIP":
+                bitSets[binaryOperation.getArg1() - 1].flip(binaryOperation.getArg2());
+                break;
+            default:
+                bitSets[binaryOperation.getArg1() - 1].set(binaryOperation.getArg2());
+                break;
+        }
+    }
+
+    private static class BinaryOperation {
+        private String binaryOperationType;
+        private int arg1;
+        private int arg2;
+
+        public BinaryOperation(String binaryOperationType, int arg1, int arg2) {
+            this.binaryOperationType = binaryOperationType;
+            this.arg1 = arg1;
+            this.arg2 = arg2;
         }
 
-        scanner.close();
+        public String getBinaryOperationType() {
+            return binaryOperationType;
+        }
+
+        public int getArg1() {
+            return arg1;
+        }
+
+        public int getArg2() {
+            return arg2;
+        }
 
     }
 }
